fce5f3ebf62195562c77b943acf04c4f5411b313
CONNOR JONATHON SODEAU <connor.sodeau.2017@mumail.ie>
2020-12-02 17:20:33 +0000

    update

--- test.py
+++ test.py
@@ -57,6 +57,7 @@ Server = server,
 Database = database)
 cursor = cnxn.cursor()
 
+
 for i in range(len(baro_altitude)):
     print ('Inserting a new row into table')
     tsql = "INSERT INTO opensky (icao24, callsign, origin_country,time_position,last_contact,longitude,latitude,geo_altitude,on_ground,velocity,heading,vertical_rate,sensors,baro_altitude,squawk,spi,position_source)VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"
@@ -64,7 +65,7 @@ for i in range(len(baro_altitude)):
     ,latitude[i],geo_altitude[i],on_ground[i],velocity[i],heading[i],vertical_rate[i],sensors[i],baro_altitude[i],
     squawk[i],spi[i],position_source[i]):
       print ('Successfully Inserted!')
-        
+        #change
     
 
 
